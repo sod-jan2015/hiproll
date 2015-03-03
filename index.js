90374b1984827c88ee1c3535a06f0cc6f6828203
Scott Nakada <scottnakada@gmail.com>
2015-03-02 17:16:57 -0800

    kkk

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 //app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
-    //response.send('Hello World Scott Nakada!');
-    response.send(cool());
+    response.send('Hello World Scott Nakada!');
+    //response.send(cool());
 });
 
 app.listen(app.get('port'), function () {
